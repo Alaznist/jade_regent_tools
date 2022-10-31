52d5b2102d9dba34240bd2e8a88101c3a0a3bc3f
Alaznist <alaznist@outlook.fr>
2022-10-31 18:46:49 +0100

    ajout du calcul des evenements

---
+++ calcul_des_evenements.py
@@ -0,0 +1,26 @@
+# Script qui permet de calculer sur quel jour aura lieu un événement pour la caravane
+
+from random import *
+
+
+jour_debut = int(input('A partir de quel jour doit on commencer les calculs ? '))
+jour_fin = int(input('A quel jour doivent-ils se terminer ? '))
+
+x = jour_debut
+
+probabilite_event_depart = 10
+probabilite_event = probabilite_event_depart
+jours_avec_events = ""
+
+while x < jour_fin :
+    dice = randint(1,100)
+    if dice <= probabilite_event:
+        nom_jour = "Jour " + str(x) 
+        jours_avec_events = jours_avec_events + "\n" + nom_jour
+        probabilite_event = probabilite_event_depart
+
+    else:
+        probabilite_event = probabilite_event + 10
+    x = x + 1
+
+print(jours_avec_events)
