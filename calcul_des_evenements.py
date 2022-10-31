59e65beb7d6eaa9a29043ad69e7d4356b6bfa247
unknown <pierrickleplenier@gmail.com>
2022-10-31 18:54:44 +0100

    add possibility to set current probability

--- calcul_des_evenements.py
+++ calcul_des_evenements.py
@@ -5,17 +5,19 @@ from random import *
 
 jour_debut = int(input('A partir de quel jour doit on commencer les calculs ? '))
 jour_fin = int(input('A quel jour doivent-ils se terminer ? '))
+probabilite_event = int(input('Quel est la probabilité actuelle d\'avoir une rencontre ?'))
+if probabilite_event < 10:
+    probabilite_event = 10
 
 x = jour_debut
 
 probabilite_event_depart = 10
-probabilite_event = probabilite_event_depart
 jours_avec_events = ""
 
 while x < jour_fin :
     dice = randint(1,100)
     if dice <= probabilite_event:
-        nom_jour = "Jour " + str(x) 
+        nom_jour = "Jour " + str(x)
         jours_avec_events = jours_avec_events + "\n" + nom_jour
         probabilite_event = probabilite_event_depart
 
